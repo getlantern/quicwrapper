bfe29e8c955d724a3a1d77e09d17faf55833b54f
Willie Forkner <1120829+forkner@users.noreply.github.com>
2018-05-07 17:47:23 -0700

    wip

---
+++ listener.go
@@ -0,0 +1,173 @@
+package quicwrapper
+
+import (
+	"crypto/tls"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	quic "github.com/lucas-clemente/quic-go"
+	"github.com/lucas-clemente/quic-go/qerr"
+)
+
+// ListenAddr creates a QUIC server listening on a given address.
+// The net.Conn instances returned by the net.Listener may be multiplexed connections.
+func ListenAddr(addr string, tlsConf *tls.Config, config *Config) (net.Listener, error) {
+
+	ql, err := quic.ListenAddr(addr, tlsConf, config)
+	if err != nil {
+		return nil, err
+	}
+
+	l := &listener{
+		quicListener: ql,
+		config:       config,
+		connections:  make(chan net.Conn, 1000),
+		acceptError:  make(chan error, 1),
+		closedSignal: make(chan struct{}),
+	}
+	go l.listen()
+	go l.logStats()
+
+	return l, nil
+}
+
+var _ net.Listener = &listener{}
+
+// wraps quic.Listener to create a net.Listener
+type listener struct {
+	quicListener          quic.Listener
+	config                *Config
+	connections           chan net.Conn
+	acceptError           chan error
+	closedSignal          chan struct{}
+	closed                bool
+	numConnections        int64
+	numVirtualConnections int64
+	mx                    sync.Mutex
+}
+
+// implements net.Listener.Accept
+func (l *listener) Accept() (net.Conn, error) {
+	select {
+	case conn, ok := <-l.connections:
+		if !ok {
+			return nil, ErrListenerClosed
+		}
+		return conn, nil
+	case err, ok := <-l.acceptError:
+		if !ok {
+			return nil, ErrListenerClosed
+		}
+		return nil, err
+	case <-l.closedSignal:
+		return nil, ErrListenerClosed
+	}
+}
+
+// implements net.Listener.Close
+// Shut down the QUIC listener.
+// this implicitly sends CONNECTION_CLOSE frames to peers
+// note: it is still the responsibility of the caller
+// to call Close() on any Conn returned from Accept()
+func (l *listener) Close() error {
+	l.mx.Lock()
+	defer l.mx.Unlock()
+	if l.closed {
+		return nil
+	}
+	l.closed = true
+
+	close(l.closedSignal)
+	err := l.quicListener.Close()
+
+	return err
+}
+
+// implements net.Listener.Addr
+func (l *listener) Addr() net.Addr {
+	return l.quicListener.Addr()
+}
+
+func (l *listener) listen() {
+	group := &sync.WaitGroup{}
+
+	defer func() {
+		l.Close()
+		close(l.acceptError)
+		// wait for writers to exit, drain connections
+		group.Wait()
+		close(l.connections)
+		for c := range l.connections {
+			c.Close()
+		}
+	}()
+
+	for {
+		session, err := l.quicListener.Accept()
+		if err != nil {
+			if !l.closed {
+				l.acceptError <- err
+			}
+			return
+		}
+		if l.closed {
+			session.Close(nil)
+			return
+		} else {
+			atomic.AddInt64(&l.numConnections, 1)
+			go l.handleSession(session, group)
+			group.Add(1)
+		}
+	}
+}
+
+func (l *listener) handleSession(session quic.Session, group *sync.WaitGroup) {
+
+	defer func() {
+		session.Close(nil)
+		atomic.AddInt64(&l.numConnections, -1)
+		group.Done()
+	}()
+
+	for {
+		stream, err := session.AcceptStream()
+		if err != nil {
+			quicErr := qerr.ToQuicError(err)
+			code := quicErr.ErrorCode
+			switch code {
+			case qerr.PeerGoingAway:
+				log.Tracef("Session closed: %s", code)
+				return
+			case qerr.NetworkIdleTimeout:
+				log.Tracef("Session closed: %s", code)
+				return
+			default:
+				log.Errorf("Error accepting stream: %s", code)
+				return
+			}
+		} else {
+			atomic.AddInt64(&l.numVirtualConnections, 1)
+			l.connections <- newConn(stream, session, l.streamClosed)
+		}
+	}
+}
+
+func (l *listener) streamClosed() {
+	atomic.AddInt64(&l.numVirtualConnections, -1)
+}
+
+func (l *listener) logStats() {
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			if !l.closed {
+				log.Debugf("Connections: %d   Virtual: %d", atomic.LoadInt64(&l.numConnections), atomic.LoadInt64(&l.numVirtualConnections))
+			}
+		case <-l.closedSignal:
+			log.Debug("Done logging stats")
+			return
+		}
+	}
+}
---
+++ quicwrapper_test.go
@@ -0,0 +1,237 @@
+package quicwrapper
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"io"
+	"math/big"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tests [][]byte
+
+func init() {
+	rb := make([]byte, 1024)
+	rand.Read(rb)
+	tests = [][]byte{
+		[]byte("x"),
+		[]byte("hello world"),
+		rb,
+		[]byte("ahoy"),
+	}
+}
+
+func dialAndEcho(t *testing.T, dialer *Client, data []byte) {
+	conn, err := dialer.Dial()
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer func() {
+		err = conn.Close()
+		assert.NoError(t, err)
+	}()
+
+	n, err := conn.Write(data)
+	if !assert.NoError(t, err) {
+		return
+	}
+	if !assert.Equal(t, len(data), n) {
+		return
+	}
+
+	buf := make([]byte, len(data))
+	_, err = io.ReadFull(conn, buf)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, data, buf)
+}
+
+func TestEchoSeq(t *testing.T) {
+	l, err := echoServer(nil)
+	assert.NoError(t, err)
+	defer l.Close()
+
+	dialer := NewClient(l.Addr().String(), &tls.Config{InsecureSkipVerify: true}, nil, nil)
+	defer dialer.Close()
+
+	for i := 0; i < 525; i++ {
+		for _, test := range tests {
+			dialAndEcho(t, dialer, test)
+		}
+	}
+}
+
+func TestEchoPar1(t *testing.T) {
+	l, err := echoServer(nil)
+	assert.NoError(t, err)
+	defer l.Close()
+
+	dialer := NewClient(l.Addr().String(), &tls.Config{InsecureSkipVerify: true}, nil, nil)
+	defer dialer.Close()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 525; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, test := range tests {
+				dialAndEcho(t, dialer, test)
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func TestEchoPar2(t *testing.T) {
+	l, err := echoServer(nil)
+	assert.NoError(t, err)
+	defer l.Close()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 25; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			dialer := NewClient(l.Addr().String(), &tls.Config{InsecureSkipVerify: true}, nil, nil)
+			defer dialer.Close()
+
+			for i := 0; i < 25; i++ {
+				for _, test := range tests {
+					dialAndEcho(t, dialer, test)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func TestEchoPar3(t *testing.T) {
+	l, err := echoServer(nil)
+	assert.NoError(t, err)
+	defer l.Close()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 25; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			dialer := NewClient(l.Addr().String(), &tls.Config{InsecureSkipVerify: true}, nil, nil)
+			defer dialer.Close()
+
+			var wg2 sync.WaitGroup
+			for j := 0; j < 25; j++ {
+				wg2.Add(1)
+				go func() {
+					defer wg2.Done()
+					for _, test := range tests {
+						dialAndEcho(t, dialer, test)
+					}
+				}()
+			}
+			wg2.Wait()
+		}()
+	}
+	wg.Wait()
+}
+
+func TestDialContextHandshakeStall(t *testing.T) {
+	l, err := stallHandshakeServer()
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer l.Close()
+
+	addr := l.LocalAddr().String()
+	dialer := NewClient(addr, &tls.Config{InsecureSkipVerify: true}, nil, nil)
+	timeout := 100 * time.Millisecond
+	errchan := make(chan error, 1)
+	var conn *Conn
+
+	go func() {
+		ctx, _ := context.WithTimeout(context.Background(), timeout)
+		c, err := dialer.DialContext(ctx)
+		conn = c
+		errchan <- err
+	}()
+
+	select {
+	case err := <-errchan:
+		assert.Nil(t, conn)
+		assert.Contains(t, err.Error(), "deadline exceeded")
+	case <-time.After(2 * timeout):
+		t.Errorf("Dial did not fail within twice timeout.")
+	}
+}
+
+// starts a server that does not complete the quic handshake
+func stallHandshakeServer() (*net.UDPConn, error) {
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	if err != nil {
+		return nil, err
+	}
+	return net.ListenUDP("udp", addr)
+}
+
+func echoServer(config *Config) (net.Listener, error) {
+
+	l, err := ListenAddr("127.0.0.1:0", generateTLSConfig(), config)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		var wg sync.WaitGroup
+		defer wg.Wait()
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				if err != ErrListenerClosed {
+					log.Errorf("accepting connection: %v", err)
+				}
+				return
+			}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err = io.Copy(conn, conn)
+				if err != nil {
+					log.Errorf("echoing data: %v", err)
+				}
+				conn.Close()
+			}()
+		}
+	}()
+
+	return l, nil
+}
+
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{Certificates: []tls.Certificate{tlsCert}}
+}
