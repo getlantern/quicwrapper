2166b395228f47a3d37015fd4913f394e184e255
Myles Horton <myles@getlantern.org>
2023-04-26 13:59:09 -0400

    kill netx test since we don't use it

--- quicwrapper_test.go
+++ quicwrapper_test.go
@@ -16,8 +16,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/getlantern/fdcount"
-	"github.com/getlantern/netx"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -147,78 +145,6 @@ func TestEchoPar3(t *testing.T) {
 		}()
 	}
 	wg.Wait()
-}
-
-func TestNetx(t *testing.T) {
-	defer netx.Reset()
-	l, err := echoServer(nil, nil)
-	assert.NoError(t, err)
-	defer l.Close()
-
-	_, port, err := net.SplitHostPort(l.Addr().String())
-	assert.NoError(t, err)
-
-	fakehost := "kjhsdafhkjsa.getiantem.org"
-	server := fmt.Sprintf("%s:%s", fakehost, port)
-
-	_, fdc, _ := fdcount.Matching("UDP")
-	defer func() {
-		err = fdc.AssertDelta(0)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	normalDialer := NewClient(server, &tls.Config{InsecureSkipVerify: true}, nil, DialWithoutNetx)
-	_, err = normalDialer.Dial()
-	assert.EqualError(t, err, fmt.Sprintf("connecting session: lookup %s: no such host", fakehost))
-	normalDialer.Close()
-
-	dialer := NewClient(server, &tls.Config{InsecureSkipVerify: true}, nil, DialWithNetx)
-	defer dialer.Close()
-
-	netx.OverrideResolveIPs(func(host string) ([]net.IP, error) {
-		host, _, err := net.SplitHostPort(host)
-		if err != nil {
-			return nil, err
-		}
-		if host == fakehost {
-			updAddr, err := net.ResolveUDPAddr("udp", l.Addr().String())
-			if err != nil {
-				return nil, err
-			}
-			return []net.IP{updAddr.IP}, err
-		} else {
-			return nil, fmt.Errorf("unexpected address %s", host)
-		}
-	})
-	/*
-		netx.OverrideResolveUDP(func(network string, addr string) (*net.UDPAddr, error) {
-			host, _, err := net.SplitHostPort(addr)
-			if err != nil {
-				return nil, err
-			}
-			if host == fakehost {
-				return net.ResolveUDPAddr(network, l.Addr().String())
-			} else {
-				return nil, fmt.Errorf("unexpected address %s", addr)
-			}
-		})
-	*/
-
-	calledListenOverride := false
-	netx.OverrideListenUDP(func(network string, laddr *net.UDPAddr) (*net.UDPConn, error) {
-		calledListenOverride = true
-		return net.ListenUDP(network, laddr)
-	})
-
-	for i := 0; i < 525; i++ {
-		for _, test := range tests {
-			dialAndEcho(t, dialer, test)
-		}
-	}
-
-	assert.True(t, calledListenOverride)
 }
 
 func TestPinnedCert(t *testing.T) {
